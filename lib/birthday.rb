6a08003e6e31a72ec2cc0c255a3f87e29667ded2
Emery Tate <tateejt@gmail.com>
2019-10-14 00:49:37 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,12 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
 end
 
 
