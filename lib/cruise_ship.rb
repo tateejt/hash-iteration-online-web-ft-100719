f5d7f024a23b00ea76401d2e524f8aa255237daf
Emery Tate <tateejt@gmail.com>
2019-10-14 00:47:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,9 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner =""
       winner = name
     end
 end
